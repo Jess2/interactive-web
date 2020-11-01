39af6bd98f460e90a466983bb1aa822702729d0e
jess2 <jess2.developer@gmail.com>
2020-11-01 20:01:21 +0900

    이미지 블렌드 추가

--- sample3/main.js
+++ sample3/main.js
@@ -36,6 +36,7 @@
   // 크기 세팅
   function setLayoutSize () {
     sceneInfo.sceneHeight = sceneInfo.sceneHeightNum * window.innerHeight;
+    sceneInfo.dom.container.style.height = `${sceneInfo.sceneHeight}px`;
   }
 
   // 애니메이션 값 계산
@@ -54,11 +55,12 @@
   }
 
   // 스크롤
-  function onScroll () {
+  function playAnimation () {
     const dom = sceneInfo.dom;
     const settings = sceneInfo.settings;
     const sceneHeight = sceneInfo.sceneHeight;
     const scrollRatio = yOffset / sceneHeight;
+    let step = 0;
 
     // 가로/세로 모두 꽉 차게 하기 위해서 여기서 세팅(계산 필요)
     const widthRatio = window.innerWidth / sceneInfo.dom.canvas.width;
@@ -83,6 +85,8 @@
 
     if (!sceneInfo.rectStartY) {
       sceneInfo.rectStartY = dom.canvas.offsetTop + (dom.canvas.height - dom.canvas.height * canvasScaleRatio) / 2; // scale이 조정된 크기를 기준으로 top의 위치 계산
+      settings.rect1.startScroll = (window.innerHeight / 2) / sceneHeight;
+      settings.rect2.startScroll = (window.innerHeight / 2) / sceneHeight;
       settings.rect1.endScroll = sceneInfo.rectStartY / sceneHeight;
       settings.rect2.endScroll = sceneInfo.rectStartY / sceneHeight;
     }
@@ -96,18 +100,63 @@
     // 좌우 박스 그리기
     dom.canvas.getContext('2d').fillRect(parseInt(calcValue(settings.rect1)), 0, whiteRectWidth, dom.canvas.height);
     dom.canvas.getContext('2d').fillRect(parseInt(calcValue(settings.rect2)), 0, whiteRectWidth, dom.canvas.height);
+
+    if (scrollRatio < settings.rect1.endScroll) { // 캔버스가 브라우저 상단에 닿기 전
+      step = 1;
+      dom.canvas.classList.remove('sticky');
+    } else { // 닿은 후
+      // 이미지 블렌드
+      step = 2;
+
+      // blendHeight: {startValue: 0, endValue: 0, startScroll: 0, endScroll: 0},
+      settings.blendHeight.startValue = 0;
+      settings.blendHeight.endValue = dom.canvas.height;
+      settings.blendHeight.startScroll = settings.rect1.endScroll;
+      settings.blendHeight.endScroll = settings.blendHeight.startScroll + 0.2;
+
+      const blendHeight = calcValue(settings.blendHeight );
+
+      dom.canvas.getContext('2d').drawImage(sceneInfo.images[1],
+          0, dom.canvas.height - blendHeight, dom.canvas.width, blendHeight,
+          0, dom.canvas.height - blendHeight, dom.canvas.width, blendHeight,
+      );
+
+
+      dom.canvas.classList.add('sticky');
+      dom.canvas.style.top = `${-(dom.canvas.height - dom.canvas.height * canvasScaleRatio) / 2}px`;
+
+      if (scrollRatio > settings.blendHeight.endScroll) {
+        step = 3;
+        settings.canvas_scale.startValue = canvasScaleRatio;
+        settings.canvas_scale.endValue = document.body.offsetWidth / (dom.canvas.width * 1.5);
+        settings.canvas_scale.startScroll = settings.blendHeight.endScroll;
+        settings.canvas_scale.endScroll = settings.canvas_scale.startScroll + 0.2;
+
+        dom.canvas.style.transform = `scale(${calcValue(settings.canvas_scale)})`;
+        dom.canvas.style.marginTop = `0px`;
+      }
+
+      if (scrollRatio > settings.canvas_scale.endScroll && settings.canvas_scale.endScroll > 0) {
+        dom.canvas.classList.remove('sticky');
+        dom.canvas.style.marginTop = `${sceneHeight * 0.4}px`;
+      }
+    }
   }
 
   window.addEventListener('scroll', () => {
     yOffset = window.pageYOffset; // 현재 스크롤 위치
-    onScroll();
+    playAnimation();
   });
 
   window.addEventListener('load', () => {
     setLayoutSize();
+    playAnimation();
   });
 
-  window.addEventListener('resize', setLayoutSize);
+  window.addEventListener('resize', () => {
+    setLayoutSize();
+    playAnimation();
+  });
 
   setCanvasImages();
   setLayoutSize();
